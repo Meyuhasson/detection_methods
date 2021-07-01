b5d51c9bf97b54cf755a0f7612d6608ada94c3c1
Bar Yamin <baryamin@gmail.com>
2021-07-01 20:25:40 +0300

    save progress

--- classifier.py
+++ classifier.py
@@ -1,17 +1,31 @@
 import numpy as np
 from feature_extraction import tfidf_extractor
 from sklearn.ensemble import RandomForestClassifier
-
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from scipy.sparse import vstack
 def main():
     random_forest = RandomForestClassifier()
     print("Getting tfidf vectors...")
-    benign_tfidf, malicious_tfidf = tfidf_extractor(2,2)
+    benign_tfidf, malicious_tfidf = tfidf_extractor(1,1)
     num_benign_samples = benign_tfidf.shape[0]
     num_malicious_samples = malicious_tfidf.shape[0]
+    benign_classifications = np.zeros((num_benign_samples,1))
+    malicious_classifications = np.ones((num_malicious_samples,1))
+
+    all_samples = vstack([benign_tfidf,malicious_tfidf]).toarray()
+    all_classes = vstack([benign_classifications, malicious_classifications]).toarray()
+
+    Xtrain, Xtest, Ytrain, Ytest = train_test_split(all_samples, all_classes, random_state=1, stratify=all_classes)
+
+
     print("Fitting RFclassifier for benign vectors...")
-    random_forest.fit(benign_tfidf,np.zeros((num_benign_samples,1)))
-    print("Fitting RFclassifier for malicious vectors...")
-    random_forest.fit(malicious_tfidf,np.ones((num_malicious_samples,1)))
+    random_forest.fit(Xtrain,Ytrain)
+
+    predict_test = random_forest.predict(Xtest)
+
+    acc_score = accuracy_score(Ytest,predict_test)
+    print(f"Accuracy Score: {acc_score}")
     print("HERE")
     
 if __name__ == "__main__":
--- feature_extraction.py
+++ feature_extraction.py
@@ -1,5 +1,10 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import js_extraction
+import pickle
+import os
+
+BENIGN_TFIDF_FNAME = "benign_tfidf.pickle"
+MALICIOUS_TFIDF_FNAME = "malicious_tfidf.pickle"
 
 def tfidf_extractor(ngramx,ngramy):
     flat_benign = []
@@ -7,26 +12,34 @@ def tfidf_extractor(ngramx,ngramy):
     list_of_vectors_benign = []
     list_of_vectors_malicious = []
 
-    print("Extracting benign...")
-    for tokens in js_extraction.get_all_js_token(True):
-        if not tokens:
-            continue
-        flat_list = []
-        for token in tokens:
-            flat_list.append(token.type)
-            flat_list.append(token.value)
-        flat_benign.append(" ".join(flat_list))
 
-    print("Extracting malicious...")
-    for tokens in js_extraction.get_all_js_token(False):
-        if not tokens:
-            continue
-        flat_list = []
-        for token in tokens:
-            flat_list.append(token.type)
-            flat_list.append(token.value)
-        flat_malicious.append(" ".join(flat_list))
+    if os.path.exists(BENIGN_TFIDF_FNAME):
+        print("Loading benign tf-idf from disk")
+        with open(BENIGN_TFIDF_FNAME,'rb') as f:
+            flat_benign = pickle.load(f)
+    else:
+        print("Extracting benign...")
+        for tokens in js_extraction.get_all_js_token(True):
+            if not tokens:
+                continue
+            flat_benign.append(" ".join([f"{token.type}_{token.value.replace(' ','-')}" for token in tokens]))
+        with open(BENIGN_TFIDF_FNAME,'wb') as f:
+            pickle.dump(flat_benign,f)
+
+    if os.path.exists(MALICIOUS_TFIDF_FNAME):
+        print("Loading malicious tf-idf from disk")
+        with open(MALICIOUS_TFIDF_FNAME,'rb') as f:
+            flat_malicious = pickle.load(f)
+    else:
+        print("Extracting malicious...")
+        for tokens in js_extraction.get_all_js_token(False):
+            if not tokens:
+                continue
+            flat_malicious.append(" ".join([f"{token.type}_{token.value.replace(' ','-')}" for token in tokens]))
+        with open(MALICIOUS_TFIDF_FNAME,'wb') as f:
+            pickle.dump(flat_malicious,f)
     
+    print("Training TfidfVectorizer")
     vectorizer = TfidfVectorizer(ngram_range=(ngramx,ngramy))
     vectorizer.fit(flat_benign)
 
